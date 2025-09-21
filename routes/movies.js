816c775c34584990df8c40555b111c1e77cf1931
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-09-21 19:54:15 +0530

    id

--- routes/movies.js
+++ routes/movies.js
@@ -22,4 +22,28 @@ router.get('/movies', async(req, res) => {
     }
 })
 
+router.get('/movies/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const movie = await Movie.findById(id)
+        res.json(movie);
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+router.get('/:movieId/theaters/:theaterId', async(req, res) => {
+    try {
+        const {movieId, theaterId} = req.params;
+        const movie = await Movie.findById(movieId)
+
+        if(!movie) return res.status(404).json({message: "Movie not found"})
+        const theatre = movie.theaters.id(theaterId)
+        if(!theatre) return res.status(404).json({message: "Theatre not found"})
+        res.json(theatre)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 export default router;
